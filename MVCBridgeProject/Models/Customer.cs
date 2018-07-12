d186cde54edc56335f8dc36d1c733fc99bf94062
JohnLaubenthal <laubenthaljohn@yahoo.com>
2018-07-12 10:11:01 -0400

    first commit

---
+++ MVCBridgeProject/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBridgeProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string FavoriteDrink { get; set; }
+        public string FavoriteMeal { get; set; }
+        public double LargestBill { get; set; }
+    }
+}
